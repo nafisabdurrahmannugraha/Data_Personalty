0de25b7ad298bd1b9d1ec62ac61b0fbbed80d06d
Tosnaeh <77777954+nafisabdurrahmannugraha@users.noreply.github.com>
2023-08-26 06:31:45 +0700

    python code
    code to input data form other

---
+++ data personality.py
@@ -0,0 +1,20 @@
+#input Username
+print("Hello, Welcome to this program, this program to input you're data personalty.")
+print("What Is youre name?")
+Username = input('My name is : ')
+print("hello", Username,"welcome to this program")
+#Optionality, yes or no
+Option_pext1 = input('Are you next the qustion?=>')
+print("thanks to next the program")
+#about he/she is born
+print("When were youre born?")
+Input_born = input('im born in Years : ')
+Input_born = float(Input_born)
+old_data = 2023-Input_born
+print("you are",old_data, "old")
+State_born = input('Where are you born?')
+print("okey, thanks for youre participant, i will send this Data to Operator, Thank you")
+
+
+
+
